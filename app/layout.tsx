b0f7600a63b4c9c5fbe5b37f51c9eccac012d025
maxielsezar <elsezarmaxi@gmail.com>
2026-04-21 18:28:19 -0300

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Cursos de Informática",
@@ -16,6 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7700991264412300"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="bg-white text-gray-900 dark:bg-black dark:text-gray-100">{children}</body>
     </html>
   );
